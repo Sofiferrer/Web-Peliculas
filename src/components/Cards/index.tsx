b2d3c8ba3af55128c9432cfa873a13c7318a3be4
Sofia Ferrer <chopotolina@gamil.com>
2021-04-21 00:01:10 -0300

    Paginador corregido, screens funcionando

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,45 +1,35 @@
-import React, { useState, useEffect, FC } from "react"
+import React, { FC } from "react"
 import './style.css';
-import { movie } from '../../api/movie'
+import noImg from '../../img/image-not-available.png'
 import { MovieType } from '../../types/MovieType'
 import { Card, ListGroup } from 'react-bootstrap'
 
 interface Props {
-    pag: number
+    pag: number;
+    moviesList: MovieType[] | undefined;
 }
 
-const Cards: FC<Props> = ({ pag }) => {
-
-    const [lastMovies, setLastMovies] = useState<MovieType[]>();
-
-    useEffect(() => {
-        movie.getLatest(pag).then((response) => {
-            setLastMovies(response);
-        })
-    }, [pag])
+const Cards: FC<Props> = ({ moviesList }) => {
 
     const imgBase = "https://image.tmdb.org/t/p/"
     const imgWidth = "w500"
 
 
     return (
-        <div className="latest-screen">
-            <h2>NUEVOS LANZAMIENTOS</h2>
-            <div className="cards-container">
-                {lastMovies && lastMovies.map((movie: MovieType) => (
-                    <div className="card-frame">
-                        <a href={"/ficha/" + movie.id}>
-                            <Card className="card">
-                                <Card.Img variant="top" className="card-img" src={imgBase + imgWidth + movie.poster_path} />
-                                <Card.Body>
-                                    <Card.Title className="card-title">{movie.title}</Card.Title>
-                                    <ListGroup.Item className="card-footer">VER FICHA</ListGroup.Item>
-                                </Card.Body>
-                            </Card>
-                        </a>
-                    </div>
-                ))}
-            </div>
+        <div className="cards-container">
+            {moviesList && moviesList.map((movie: MovieType) => (
+                <div className="card-frame">
+                    <a href={"/ficha/" + movie.id}>
+                        <Card className="card">
+                            <Card.Img variant="top" className="card-img" src={movie.poster_path ? imgBase + imgWidth + movie.poster_path : noImg} />
+                            <Card.Body>
+                                <Card.Title className="card-title">{movie.title}</Card.Title>
+                                <ListGroup.Item className="card-footer">VER FICHA</ListGroup.Item>
+                            </Card.Body>
+                        </Card>
+                    </a>
+                </div>
+            ))}
         </div>
     )
 }
